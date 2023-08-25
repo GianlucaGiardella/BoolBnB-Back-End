93e3cb92fe1b67b8ef283dfab5efbecc7c739124
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-08-25 11:34:11 +0200

    add message migration

---
+++ database/migrations/2023_08_25_091442_create_messages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            
+            $table->varchar('email_sender', 30);
+            $table->text('text_message');
+            $table->datetime('sent_date');
+            $table->timestamps();
+            $table->foreignId('apartment_id');
+            $table->foreign('apartment_id')->references('id')->on('apartment');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+};
