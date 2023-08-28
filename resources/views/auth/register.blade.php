b6af1b747362f3f5d60cfd21f26fbc8205036d3c
GianluGiarde <gianlugiarde@gmail.com>
2023-08-28 12:35:44 +0200

    fix register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
         @csrf
 
         <div class="mb-3">
-            <label for="name" class="form-label">Name</label>
+            <label for="name" class="form-label">Name *</label>
             <input type="text" class="form-control" id="name" name="name" required autofocus autocomplete="name"
                 value="{{ old('name') }}">
         </div>
@@ -17,24 +17,24 @@
         </div>
 
         <div class="mb-3">
-            <label for="name" class="form-label">Date od Birth</label>
+            <label for="name" class="form-label">Date of Birth</label>
             <input type="date" class="form-control" id="birth_date" name="birth_date" autofocus autocomplete="birth_date"
                 value="{{ old('birth_date') }}">
         </div>
 
         <div class="mb-3">
-            <label for="email" class="form-label">Email</label>
+            <label for="email" class="form-label">Email *</label>
             <input type="email" class="form-control" id="email" name="email" required autofocus
                 autocomplete="username" value="{{ old('email') }}">
         </div>
 
         <div class="mb-3">
-            <label for="password" class="form-label">Password</label>
+            <label for="password" class="form-label">Password *</label>
             <input type="password" class="form-control" id="password" name="password" required autocomplete="new-password">
         </div>
 
         <div class="mb-3">
-            <label for="password" class="form-label">Confirm Password</label>
+            <label for="password" class="form-label">Confirm Password *</label>
             <input type="password" class="form-control" id="password" name="password_confirmation" required
                 autocomplete="new-password">
         </div>
