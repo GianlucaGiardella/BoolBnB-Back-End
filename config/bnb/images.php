0c8bd53e7af8182e6ab26fef0d40f20e84cd292f
FeliceDArcangelo <felicedarcangelo25@gmail.com>
2023-08-29 18:11:09 +0200

    add apartments address

--- config/bnb/images.php
+++ config/bnb/images.php
@@ -4,51 +4,51 @@ return [
     [
         "id" => 1,
         "apartment_id" => 1,
-        "img_url" => '',
+        "img_url" => 'https://images.squarespace-cdn.com/content/v1/60c2d7b34353426a7c4a1765/1628225522225-J0MMMS3BO1GYP6UFMGW3/66a01b2c-3b21-40ae-982f-87a87c02cd9c.jpg?format=1500w',
     ],
-    // [
-    //     "id" => 2,
-    //     "apartment_id" => [],
-    //     "img_url" => [],
-    // ],
-    // [
-    //     "id" => 3,
-    //     "apartment_id" => [],
-    //     "img_url" => [],
-    // ],
-    // [
-    //     "id" => 4,
-    //     "apartment_id" => [],
-    //     "img_url" => [],
-    // ],
-    // [
-    //     "id" => 5,
-    //     "apartment_id" => [],
-    //     "img_url" => [],
-    // ],
-    // [
-    //     "id" => 6,
-    //     "apartment_id" => [],
-    //     "img_url" => [],
-    // ],
-    // [
-    //     "id" => 7,
-    //     "apartment_id" => [],
-    //     "img_url" => [],
-    // ],
-    // [
-    //     "id" => 8,
-    //     "apartment_id" => [],
-    //     "img_url" => [],
-    // ],
-    // [
-    //     "id" => 9,
-    //     "apartment_id" => [],
-    //     "img_url" => [],
-    // ],
-    // [
-    //     "id" => 10,
-    //     "apartment_id" => [],
-    //     "img_url" => [],
-    // ],
+    [
+        "id" => 2,
+        "apartment_id" => 1,
+        "img_url" => 'https://images.squarespace-cdn.com/content/v1/60c2d7b34353426a7c4a1765/1628225523864-JDJCU2GFEFQYVZQRDQ9P/c4b1a312-2fff-4be6-b84b-11876aadf8d4.jpg?format=1500w',
+    ],
+    [
+        "id" => 3,
+        "apartment_id" => 1,
+        "img_url" => 'https://images.squarespace-cdn.com/content/v1/60c2d7b34353426a7c4a1765/1628225520515-9GG67Z4Q25WJ11DH6CBS/6c95c60c-1c34-4bf9-bb16-a9f13cf76682.jpg?format=1500w',
+    ],
+    [
+        "id" => 4,
+        "apartment_id" => 1,
+        "img_url" => 'https://images.squarespace-cdn.com/content/v1/60c2d7b34353426a7c4a1765/1628225530904-4RZJX7XZSDV1NDGQZEXT/a65c4991-7d5c-43c4-bab0-c23b464368c7.jpg?format=1500w',
+    ],
+    [
+        "id" => 5,
+        "apartment_id" => 1,
+        "img_url" => 'https://images.squarespace-cdn.com/content/v1/60c2d7b34353426a7c4a1765/1628225529345-PMWPQ2WCN35FYWI6UETW/0f9d9beb-5a59-4dbd-aba3-6a2aaf2ee222.jpg?format=1500w',
+    ],
+    [
+        "id" => 6,
+        "apartment_id" => [],
+        "img_url" => [],
+    ],
+    [
+        "id" => 7,
+        "apartment_id" => [],
+        "img_url" => [],
+    ],
+    [
+        "id" => 8,
+        "apartment_id" => [],
+        "img_url" => [],
+    ],
+    [
+        "id" => 9,
+        "apartment_id" => [],
+        "img_url" => [],
+    ],
+    [
+        "id" => 10,
+        "apartment_id" => [],
+        "img_url" => [],
+    ],
 ];
