509ba5795323298688c10c2aece93b726b72ff05
Andreaserra1997 <serra.andrea1997@gmail.com>
2023-08-29 12:30:58 +0200

    validation js

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
                 <p class="title text-gradient">Crea un Account</p>
             </div>
             <div class="input_container">
-                <label for="name" class="input_label">Nome *</label>
+                <label for="name" class="input_label">Nome</label>
                 <input type="text" class="input_field @error('name') is-invalid @enderror" id="name" name="name"
                     autocomplete="name" value="{{ old('name') }}">
                 @error('name')
@@ -40,7 +40,7 @@
             <div class="input_container">
                 <label class="input_label" for="email">Email *</label>
                 <svg fill="none" viewBox="0 0 24 24" height="24" width="24" xmlns="http://www.w3.org/2000/svg"
-                    class="icon">
+                    class="icon" required>
                     <path stroke-linejoin="round" stroke-linecap="round" stroke-width="1.5" stroke="#141B34"
                         d="M7 8.5L9.94202 10.2394C11.6572 11.2535 12.3428 11.2535 14.058 10.2394L17 8.5"></path>
                     <path stroke-linejoin="round" stroke-width="1.5" stroke="#141B34"
@@ -49,7 +49,7 @@
                 </svg>
                 <input type="email" class="input_field @error('email') is-invalid @enderror" id="email"
                     aria-describedby="emailHelp" name="email" placeholder="name@mail.com" autocomplete="username"
-                    value="{{ old('email') }}">
+                    value="{{ old('email') }}" required minlength="8">
                 @error('email')
                     <div class="invalid-feedback">
                         {{ $message }}
@@ -59,7 +59,7 @@
             <div class="input_container">
                 <label class="input_label" for="password">Password *</label>
                 <svg fill="none" viewBox="0 0 24 24" height="24" width="24" xmlns="http://www.w3.org/2000/svg"
-                    class="icon">
+                    class="icon" required minlength="8">
                     <path stroke-linecap="round" stroke-width="1.5" stroke="#141B34"
                         d="M18 11.0041C17.4166 9.91704 16.273 9.15775 14.9519 9.0993C13.477 9.03404 11.9788 9 10.329 9C8.67911 9 7.18091 9.03404 5.70604 9.0993C3.95328 9.17685 2.51295 10.4881 2.27882 12.1618C2.12602 13.2541 2 14.3734 2 15.5134C2 16.6534 2.12602 17.7727 2.27882 18.865C2.51295 20.5387 3.95328 21.8499 5.70604 21.9275C6.42013 21.9591 7.26041 21.9834 8 22">
                     </path>
@@ -70,7 +70,7 @@
                     </path>
                 </svg>
                 <input type="password" class="input_field @error('password') is-invalid @enderror" id="password"
-                    name="password" placeholder="Password" autocomplete="current-password">
+                    name="password" placeholder="Password" autocomplete="current-password" required minlength="8">
                 @error('password')
                     <div class="invalid-feedback">
                         {{ $message }}
@@ -90,7 +90,7 @@
                         d="M21.2046 15.1045L20.6242 15.6956V15.6956L21.2046 15.1045ZM21.4196 16.4767C21.7461 16.7972 22.2706 16.7924 22.5911 16.466C22.9116 16.1395 22.9068 15.615 22.5804 15.2945L21.4196 16.4767ZM18.0228 15.1045L17.4424 14.5134V14.5134L18.0228 15.1045ZM18.2379 18.0387C18.5643 18.3593 19.0888 18.3545 19.4094 18.028C19.7299 17.7016 19.7251 17.1771 19.3987 16.8565L18.2379 18.0387ZM14.2603 20.7619C13.7039 21.3082 12.7957 21.3082 12.2394 20.7619L11.0786 21.9441C12.2794 23.1232 14.2202 23.1232 15.4211 21.9441L14.2603 20.7619ZM12.2394 20.7619C11.6914 20.2239 11.6914 19.358 12.2394 18.82L11.0786 17.6378C9.86927 18.8252 9.86927 20.7567 11.0786 21.9441L12.2394 20.7619ZM12.2394 18.82C12.7957 18.2737 13.7039 18.2737 14.2603 18.82L15.4211 17.6378C14.2202 16.4587 12.2794 16.4587 11.0786 17.6378L12.2394 18.82ZM14.2603 18.82C14.8082 19.358 14.8082 20.2239 14.2603 20.7619L15.4211 21.9441C16.6304 20.7567 16.6304 18.8252 15.4211 17.6378L14.2603 18.82ZM20.6242 15.6956L21.4196 16.4767L22.5804 15.2945L21.785 14.5134L20.6242 15.6956ZM15.4211 18.82L17.8078 16.4767L16.647 15.2944L14.2603 17.6377L15.4211 18.82ZM17.8078 16.4767L18.6032 15.6956L17.4424 14.5134L16.647 15.2945L17.8078 16.4767ZM16.647 16.4767L18.2379 18.0387L19.3987 16.8565L17.8078 15.2945L16.647 16.4767ZM21.785 14.5134C21.4266 14.1616 21.0998 13.8383 20.7993 13.6131C20.4791 13.3732 20.096 13.1716 19.6137 13.1716V14.8284C19.6145 14.8284 19.619 14.8273 19.6395 14.8357C19.6663 14.8466 19.7183 14.8735 19.806 14.9391C19.9969 15.0822 20.2326 15.3112 20.6242 15.6956L21.785 14.5134ZM18.6032 15.6956C18.9948 15.3112 19.2305 15.0822 19.4215 14.9391C19.5091 14.8735 19.5611 14.8466 19.5879 14.8357C19.6084 14.8273 19.6129 14.8284 19.6137 14.8284V13.1716C19.1314 13.1716 18.7483 13.3732 18.4281 13.6131C18.1276 13.8383 17.8008 14.1616 17.4424 14.5134L18.6032 15.6956Z">
                     </path>
                 </svg>
-                <input type="password" class="input_field @error('password') is-invalid @enderror" id="password"
+                <input type="password" class="input_field @error('password') is-invalid @enderror" id="confirmPassword"
                     name="password_confirmation" placeholder="Password" autocomplete="new-password">
                 @error('password')
                     <div class="invalid-feedback">
@@ -106,3 +106,20 @@
         </form>
     </div>
 @endsection
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        let form = document.querySelector("form");
+    
+        form.addEventListener("submit", function(event) {
+        let password = document.getElementById("password").value;
+        let confirmPassword = document.getElementById("confirmPassword").value;
+    
+        // Verificare se la conferma password corrisponde alla password inserita prima
+        if (password !== confirmPassword) {
+            alert("Le password non coincidono!");
+            event.preventDefault();
+            return;
+        }
+        });
+    });
+</script>
