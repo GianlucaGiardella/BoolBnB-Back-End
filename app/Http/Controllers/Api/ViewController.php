7abf1df748cae024eb6a37df2fea730125cf3c81
GianluGiarde <gianlugiarde@gmail.com>
2023-08-29 22:06:11 +0200

    remove comments

--- app/Http/Controllers/Api/ViewController.php
+++ app/Http/Controllers/Api/ViewController.php
@@ -8,78 +8,37 @@ use Illuminate\Http\Request;
 
 class ViewController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $view = View::all();
         return response()->json($view);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\View  $view
-     * @return \Illuminate\Http\Response
-     */
     public function show(View $view)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\View  $view
-     * @return \Illuminate\Http\Response
-     */
     public function edit(View $view)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\View  $view
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, View $view)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\View  $view
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(View $view)
     {
         //
