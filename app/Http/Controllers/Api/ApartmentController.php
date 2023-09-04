0673c2820010d0da9b9f8cf0b8a4b14e343f2909
GianluGiarde <gianlugiarde@gmail.com>
2023-09-04 18:52:04 +0200

    fix api controller

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -2,24 +2,39 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use App\Models\Apartment;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class ApartmentController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         // $apartment = Apartment::all();
-
         // return response()->json([
         //     'success'   => true,
         //     'results'   => $apartment,
         // ]);
-        $apartment = Apartment::with(["images", "services"])->get();
+
+        // $apartments = Apartment::with(["images", "services"])->get();
+        // return response()->json([
+        //     'success'   => true,
+        //     'results'   => $apartments,
+        // ]);
+
+        $selectedServices = $request->input('services');
+
+        if ($selectedServices) {
+            $apartments = Apartment::whereHas('services', function ($query) use ($selectedServices) {
+                $query->whereIn('id', $selectedServices);
+            })->get();
+        } else {
+            $apartments = Apartment::with(['user', 'services', 'images', 'sponsors'])->get();
+        }
+
         return response()->json([
             'success'   => true,
-            'results'   => $apartment,
+            'results'   => $apartments,
         ]);
     }
 
@@ -37,13 +52,12 @@ class ApartmentController extends Controller
     {
         // $apartment = Apartment::where('slug', $slug)->firstOrFail();
 
-        $apartment = Apartment::where("slug", $slug)->with(["images","services"])->firstOrFail();
+        $apartment = Apartment::where("slug", $slug)->with(["images", "services"])->firstOrFail();
 
         return response()->json([
             'success'   => $apartment ? true : false,
             'results'   => $apartment,
         ]);
-
     }
 
     public function edit(Apartment $apartment)
