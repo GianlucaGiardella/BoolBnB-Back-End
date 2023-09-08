4124f9d2f728de1f4caa1f1179d6bf7258e46854
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-09-08 19:02:39 +0200

    add error page

--- resources/views/admin/payment/error.blade.php
+++ resources/views/admin/payment/error.blade.php
@@ -1,1 +1,12 @@
-ERROREEEEEEEEE
+@extends('layouts.base')
+@section('contents')
+    <div class="my-3">
+        <div class="row">
+            <div class="card">
+                <div class="card-body text-center">
+                    <h1>Si è verificato un errore ritenta e sarai più fortunato</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
