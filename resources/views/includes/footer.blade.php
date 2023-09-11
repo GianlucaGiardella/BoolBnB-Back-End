5bfcf4eee3deeb01d1fabab6a7fb1a5fdb37eaae
GianluGiarde <gianlugiarde@gmail.com>
2023-09-11 17:34:31 +0200

    fix footer responsive

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -26,24 +26,27 @@
     </p>
 
     <div class="container">
-        <div class="d-flex flex-wrap justify-content-between align-items-center py-3 border-top">
+        <div class="footer-text d-flex justify-content-between align-items-center py-3 border-top">
+
+            <!-- Copyrigth -->
             <p class="col-md-4 mb-0 text-muted">© 2023 Boolean, Classe 96</p>
 
             <!-- Logo -->
-            <a class="d-flex justify-content-center" href="http://localhost:5174">
+            <a class="logo d-flex justify-content-center" href="http://localhost:5174">
                 <img class="full-logo" src="{{ url('/logos/multicolor-horizontal-logo.png') }}" alt="">
                 <img class="icon-logo" src="{{ url('/logos/multicolor-logo.ico') }}" alt="">
             </a>
 
-            <ul class="nav col-md-4 justify-content-end">
-                <li class="nav-item"><a href="http://localhost:5174"
-                        class="px-2 text-black link-unstyled nav-link">Home</a></li>
-                <li class="nav-item"><a href="http://localhost:5174/search"
-                        class="px-2 text-black link-unstyled nav-link">Cerca</a></li>
-                <li class="nav-item"><a href="http://localhost:5174/aboutus"
-                        class="px-2 text-black link-unstyled nav-link">About</a></li>
-                <li class="nav-item"><a href="{{ route('admin.dashboard') }}"
-                        class="px-2 text-black link-unstyled nav-link">Profilo</a></li>
+            <!-- Link -->
+            <ul class="row row-cols-4 justify-content-between col-md-4 m-0 p-0 list-unstyled">
+                <li class="nav-item px-1"><a href="http://localhost:5174"
+                        class="px-1 text-black link-unstyled nav-link">Home</a></li>
+                <li class="nav-item px-1"><a href="http://localhost:5174/search"
+                        class="px-1 text-black link-unstyled nav-link">Cerca</a></li>
+                <li class="nav-item px-1"><a href="http://localhost:5174/aboutus"
+                        class="px-1 text-black link-unstyled nav-link">About</a></li>
+                <li class="nav-item px-1"><a href="{{ route('admin.dashboard') }}"
+                        class="px-1 text-black link-unstyled nav-link">Profilo</a></li>
             </ul>
         </div>
     </div>
@@ -55,7 +58,35 @@
         box-shadow: 0px -2px 10px -2px rgba(0, 0, 0, 0.3);
     }
 
-    .full-logo {
-        height: 30px;
+    .full-logo,
+    .icon-logo {
+        height: 40px;
+    }
+
+    @media (max-width: 991px) {
+
+        .full-logo {
+            display: none !important;
+        }
+
+        .icon-logo {
+            display: block !important;
+            width: 40px;
+            height: 40px;
+        }
+    }
+
+    @media (max-width: 767px) {
+
+        .logo {
+            display: none !important;
+        }
+    }
+
+    @media (max-width: 489px) {
+        .footer-text {
+            flex-direction: column;
+
+        }
     }
 </style>
