e2abdee8fbab3fa8126dfb3b6cb8c4bfa229d96a
FeliceDArcangelo <felicedarcangelo25@gmail.com>
2023-09-12 09:48:51 +0200

    inserimento messaggi

--- config/bnb/messages.php
+++ config/bnb/messages.php
@@ -7,4 +7,34 @@ return [
 		"email_sender" => "lkjh@lkjh.lkjh",
 		"text_message" => "Ciao, come stai?",
 	],
+	[
+		"id" => 3,
+		"apartment_id" => 9,
+		"email_sender" => "marco@gmail.com",
+		"text_message" => "Ciao, la contattatto per info sui prezzi",
+	],
+	[
+		"id" => 3,
+		"apartment_id" => 8,
+		"email_sender" => "giuseppe@hotmail.com",
+		"text_message" => "Ciao, è possibile prenotare per la seconda settimana di ottobre?",
+	],
+	[
+		"id" => 1,
+		"apartment_id" => 16,
+		"email_sender" => "annamaria@gmail.com",
+		"text_message" => "Ciao, potrei avere qualche altra forto dell'appartamento?",
+	],
+	[
+		"id" => 1,
+		"apartment_id" => 17,
+		"email_sender" => "giovanna@gmail.com",
+		"text_message" => "Ciao, è disponibile il servizio di baby-sitting?",
+	],
+	[
+		"id" => 1,
+		"apartment_id" => 19,
+		"email_sender" => "antonio@gmail.com",
+		"text_message" => "Ciao, avete qualche promozione per dicembre?",
+	],
 ];
